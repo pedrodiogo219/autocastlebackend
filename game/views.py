c995e7421b4b89c86ae00e4b7d42ab1c7696f5c1
Pedro Diogo <pedrodiogo219@gmail.com>
2022-10-14 18:13:57 -0300

    adding mate in 6 position

--- game/views.py
+++ game/views.py
@@ -9,20 +9,25 @@ def index(request):
     return HttpResponse("Teste")
 
 
-
-
 def proccessGame():
-    # print(stockfish.get_parameters())
+    print(stockfish.get_parameters())
     # print(stockfish)
-    stockfish.set_fen_position("rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2")
+    stockfish.set_fen_position("r1bk3q/pp1p4/nbp3p1/4N3/3P1p2/6P1/PPP4P/R1B1R1K1 w - - 0 2")
 
+    moves = []
     while True:
         print(stockfish.get_board_visual())
 
         move = stockfish.get_best_move()
-        print(move)
+        if not move:
+            break
+
+        moves.append(move)
 
         stockfish.make_moves_from_current_position([move])
+    
+    print(moves)
+    return moves
 
 
 
