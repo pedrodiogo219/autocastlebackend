e79f223b5f65fa4de08297dc3d5bcf804ffbcab4
Pedro Diogo <p.diogo@eyeo.com>
2023-10-01 21:14:09 +0200

    accepting any position and limiting to 100 moves

--- game/views.py
+++ game/views.py
@@ -5,14 +5,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from game import stockfish
 
+INITIAL_POSITION = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+CHECKMATE_IN_ONE_UNDERPROMOTION = "8/5k1P/8/5K2/8/B7/8/4R1R1 w - - 0 1"
+PROMOTION_CHECKMATE = "1k6/7P/1K6/8/8/8/8/8 w - - 0 1"
+CHECKMATE_IN_FIVE = "r1bk3q/pp1p4/nbp3p1/4N3/3P1p2/6P1/PPP4P/R1B1R1K1 w - - 0 2"
 
 class gameView(APIView):
     queryset = User.objects.none() 
     def get(self, request):
-        # position = "8/5k1P/8/5K2/8/B7/8/4R1R1 w - - 0 1"
-        # position = "1k6/7P/1K6/8/8/8/8/8 w - - 0 1"
-        # position = "r1bk3q/pp1p4/nbp3p1/4N3/3P1p2/6P1/PPP4P/R1B1R1K1 w - - 0 2"
-        position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+        position = request.GET.get('position') or INITIAL_POSITION
         states = proccessGame(position)
         return Response({'states': states, 'position': position})    
 
@@ -30,7 +31,8 @@ def proccessGame(position):
     # print(stockfish.get_board_visual())
 
     states = [position]
-    while True:
+    max_moves = 100
+    for _ in range(max_moves):
         move = stockfish.get_best_move()
         if not move:
             break
